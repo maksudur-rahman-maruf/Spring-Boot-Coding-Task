43a403bb47a59342f75b3268de88bc88cce5fd6c
Maksudur Rahman Maruf <43263023+maksudur-rahman-maruf@users.noreply.github.com>
2021-10-19 22:13:33 +0600

    added Repository Annotation in UserRepository

--- src/main/java/com/maruf/springboottask/repository/UserRepository.java
+++ src/main/java/com/maruf/springboottask/repository/UserRepository.java
@@ -1,16 +1,15 @@
 package com.maruf.springboottask.repository;
 
-
-
 import com.maruf.springboottask.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 
-public interface UserRepository extends JpaRepository<User, Integer>
-{
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 
-	Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 
 }
