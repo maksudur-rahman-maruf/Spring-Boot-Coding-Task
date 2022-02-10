732c6315eb6c06197cf970c4650ad6f095403b29
Maksudur Rahman Maruf <43263023+maksudur-rahman-maruf@users.noreply.github.com>
2022-02-10 17:00:24 +0600

    Update ContactRestController.java

--- src/main/java/com/maruf/springboottask/controller/ContactRestController.java
+++ src/main/java/com/maruf/springboottask/controller/ContactRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -16,11 +17,13 @@ public class ContactRestController {
     private ContactRepository contactRepository;
 
     @PostMapping()
+    @RolesAllowed("ADMIN")
     public Contact createContact(@RequestBody Contact contact) {
         return contactRepository.save(contact);
     }
 
     @GetMapping()
+    @RolesAllowed({"ADMIN", "USER"})
     public List<Contact> getContact() {
         return contactRepository.findAll();
     }
